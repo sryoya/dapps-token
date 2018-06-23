11d0b5ecff28bc70efb61822200c10aa72aafde9
sryoya <sryoya0814@gmail.com>
2018-06-23 21:51:26 +0900

    first commit

---
+++ contracts/DappsToken.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.18;
+
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+contract DappsToken is StandardToken {
+
+    string public name = "DappsToken";
+
+    string public symbol = "DTKN";
+
+    uint public decimals = 18;
+
+    constructor(uint initialSupply) public {
+        totalSupply_ = initialSupply;
+        balances[msg.sender] = initialSupply;
+    }
+
+
+}
